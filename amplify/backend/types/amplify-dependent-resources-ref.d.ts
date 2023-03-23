df14da974a2edf7002b7f3a3ce075031d8f42ce6
Jim O'Mulloy <jimomulloy@gmail.com>
2023-03-23 11:40:47 +0000

    tutorial complete

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "instrumentampera289ff0f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Storage } from 'aws-amplify';
 
 function App() {
+  const [file, setFile] = useState();
+  const [uploaded, setUploaded] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      <button onClick={async () => {
+        const storageResult = await Storage.put('puppy.png', file, {
+          level: 'public',
+          type: 'image/png'
+        })
+        // Insert predictions code here later
+        setUploaded(true)
+        console.log(storageResult);
+      }}>Upload and check if there's a dog!</button>
+
+      <div>
+        {uploaded
+          ? <div>Your image is uploaded!</div>
+          : <div>Upload a photo to get started</div>}
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
