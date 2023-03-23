9fa4caca7d60260dc1bf5aab376e2b80c8c8cd42
Jim O'Mulloy <jimomulloy@gmail.com>
2023-03-23 14:05:54 +0000

    react auth ui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import { useState } from 'react'
 import { Storage } from 'aws-amplify';
+import { withAuthenticator, Button, Heading, View } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({ signOut, user }) { 
   const [file, setFile] = useState();
   const [uploaded, setUploaded] = useState(false);
 
   return (
-    <div className="App">
+    <View style={styles.container}>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <h2>Instrument Amp</h2>
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
       <button onClick={async () => {
-        const storageResult = await Storage.put('puppy.png', file, {
-          level: 'public',
-          type: 'image/png'
+        const storageResult = await Storage.put('input/test.wav', file, {
+          level: 'private',
+          type: 'audio/wav'
         })
         // Insert predictions code here later
         setUploaded(true)
         console.log(storageResult);
-      }}>Upload and check if there's a dog!</button>
+      }}>Upload and check if there's a midi created!</button>
 
-      <div>
+      <View>
         {uploaded
-          ? <div>Your image is uploaded!</div>
-          : <div>Upload a photo to get started</div>}
-      </div>
-    </div>
+          ? <div>Your Audio file is uploaded!</div>
+          : <div>Upload an Audio WAV file to get started</div>}
+      </View>
+    </View>
     );
 }
 
-export default App;
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
+export default withAuthenticator(App);
