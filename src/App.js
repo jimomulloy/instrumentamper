2de04b2d167ffcb7c8deaad7b9a3f5f57936abbc
Jim O'Mulloy <jimomulloy@gmail.com>
2023-03-27 08:39:11 +0100

    fix use state update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import { Auth, Storage } from 'aws-amplify';
-import { withAuthenticator, Button, Heading, View, SelectField, Card, Flex, Divider, Collection, Link, SwitchField} from '@aws-amplify/ui-react';
+import { withAuthenticator, Button, Heading, Text, SelectField, Flex, Divider, Link, SwitchField} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-//import MidiPlayer from 'react-midi-player';
-import MidiPlayer from 'web-midi-player';
+import MidiPlayer from 'react-midi-player';
 import * as MidiConvert from 'midiconvert';
 import * as Tone from 'tone'
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
@@ -11,10 +10,11 @@ import ReactAudioPlayer from 'react-audio-player';
 
 function App({ signOut, user }) { 
   const [file, setFile] = useState();
-  const [volume, setVolume] = useState(50);
   const [paramStyle, setParamStyle] = useState('default');
   const [uploaded, setUploaded] = useState(false);
+  const [midiTrackReady, setMidiTrackReady] = useState(false);
   const [audioFileReady, setAudioFileReady] = useState(false);
+  const [midiMasterFile, setMidiMasterFile] = useState(null);
   const [midiFile, setMidiFile] = useState(null);
   const [midiPlayOnLoad, setMidiPlayOnLoad] = useState(false);
   const [midiItems, setMidiItems] = useState([]);
@@ -31,6 +31,39 @@ function App({ signOut, user }) {
     setRecordState(RecordState.STOP);
   }
 
+  function MidiTrackSelector(props) {
+    return (
+      <Flex>  
+        <SelectField
+          label="Midi Tracks"
+          onChange={(e) => playMidiTrack(e.target.value)}
+          options={props.items.map(item => item.trackName)}
+        ></SelectField>  
+      </Flex>);
+  }
+
+  const playOnLoad = () => {
+    if (midiPlayOnLoad && midiMasterFile) {
+      var synth = new Tone.PolySynth().toDestination()
+      
+      MidiConvert.load(midiMasterFile, function(midi) {
+        // make sure you set the tempo before you schedule the events
+        Tone.Transport.bpm.value = midi.header.bpm
+      
+        // pass in the note events from one of the tracks as the second argument to Tone.Part 
+        var midiPart = new Tone.Part(function(time, note) {
+      
+          //use the events to play the synth
+          synth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+      
+        }, midi.tracks[0].notes).start()
+      
+        // start the transport to hear the events
+        Tone.Transport.start()
+      })
+    }  
+  }
+
   //audioData contains blob and blobUrl
   const onStop = (audioData) => {
     setRecordData(audioData);
@@ -39,37 +72,27 @@ function App({ signOut, user }) {
     console.log('audioData', audioData);
   }
 
-  const getMidiFile = midiFile => {
-    if (midiFile.key.endsWith('.midi') || midiFile.key.endsWith('.mid')) {
-      Storage.get(midiFile.key, {
+  const playMidiTrack = trackName => {
+    console.log(midiItems[0]);
+    const item = midiItems.filter(item => item.trackName === trackName)[0];
+    if (item !== null) {
+      console.log(item);
+      setMidiFile(item.midiFile);
+      setMidiTrackReady(true);
+    }  
+  }
+  const loadMidiFile = item => {
+    if (item.key.endsWith('.midi') || item.key.endsWith('.mid')) {
+      Storage.get(item.key, {
         level: 'private'
       }).then(result => {
         console.log(result)
         setMidiFile(result);
-        const trackName = midiFile.key.split('/')[1].split('.')[0];
-        const midiPlayer = new MidiPlayer();
-        midiItems.push({midiFile: result, key: midiFile.key, muted: false, trackName: trackName, midiPlayer: midiPlayer});
-
-        if (midiPlayOnLoad) {
-          var synth = new Tone.PolySynth().toDestination()
-          
-          MidiConvert.load(result, function(midi) {
-          
-            // make sure you set the tempo before you schedule the events
-            Tone.Transport.bpm.value = midi.header.bpm
-          
-            // pass in the note events from one of the tracks as the second argument to Tone.Part 
-            var midiPart = new Tone.Part(function(time, note) {
-          
-              //use the events to play the synth
-              synth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
-          
-            }, midi.tracks[0].notes).start()
-          
-            // start the transport to hear the events
-            Tone.Transport.start()
-          })
-        }  
+        const trackName = item.key.split('/')[1].split('.')[0];
+        if (!trackName.includes('track')) {
+          setMidiMasterFile(result);
+        } 
+        setMidiItems(midiItems => [...midiItems, {midiFile: result, key: item.key, muted: false, trackName: trackName}]);
       })
     }  
   }
@@ -115,6 +138,9 @@ function App({ signOut, user }) {
         <Flex direction="row" alignItems="center">
           <Button onClick={async () => {
             setMidiItems([]);
+            setMidiMasterFile(null);
+            setMidiFile(null);
+            setMidiTrackReady(false);
             console.log(file)
             const storageResult = await Storage.put('input/' + file.name, file, {
               metadata: { 'x-amz-meta-instrument-style': paramStyle},
@@ -126,6 +152,9 @@ function App({ signOut, user }) {
           }}>Upload File</Button>
           <Button onClick={async () => {
             setMidiItems([]);
+            setMidiMasterFile(null);
+            setMidiFile(null);
+            setMidiTrackReady(false);
             console.log(recordData)
             const storageResult = await Storage.put('input/' + 'recording.wav', recordData.blob, {
               metadata: { 'x-amz-meta-instrument-style': paramStyle},
@@ -139,95 +168,77 @@ function App({ signOut, user }) {
       </Flex>
       <Flex direction="column" gap="1rem" alignItems="center">  
         <SelectField
-              label="Parameter Style"
-              value={paramStyle}
-              onChange={(e) => setParamStyle(e.target.value)}
-            >
-              <option value="default">Default</option>
-              <option value="voice">Voice</option>
-              <option value="guitar">Guitar</option>
-              <option value="piano">Piano</option>
-              <option value="ensemble">Ensemble</option>
-            </SelectField>  
+          label="Parameter Style"
+          value={paramStyle}
+          onChange={(e) => setParamStyle(e.target.value)}
+        >
+          <option value="default">Default</option>
+          <option value="voice">Voice</option>
+          <option value="guitar">Guitar</option>
+          <option value="piano">Piano</option>
+          <option value="ensemble">Ensemble</option>
+        </SelectField>  
       </Flex>      
       <Divider
           orientation="horizontal" />
       <Flex direction="column" gap="1rem" alignItems="center" alignContent="center">
-          <Heading level={4}>Midi Output</Heading>
-          <Flex>
-            <Button onClick={async () => {
-              const session = await Auth.currentSession()
-              console.log(session);
-              const result = await Storage.list('output/', {
-                level: 'private',
-                type: 'audio/midi'
-              })
-              console.log(result);
-              result.results.forEach(item => {
-                getMidiFile(item);
-              })
-            }}>Load MIDI file</Button>
-            <SwitchField
-              label="Play OnLoad"
-              isChecked={midiPlayOnLoad}
-              onChange={(e) => {
-                setMidiPlayOnLoad(e.target.checked);
-              }}
-              />
-          </Flex>    
-          <Heading level={5}>Midi Tracks</Heading>
-          <Collection
-            items={midiItems}
-            type="list"
-            direction="column"
-            gap="20px"
-            wrap="nowrap"
-          >
-            {(item, index) => (
-              <Card
-                key={index}
-                borderRadius="medium"
-                maxWidth="20rem"
-                variation="outlined"
-              >
-                <View padding="xs">
-                  <Flex>
-                    <Button
-                           onClick={() =>
-                             item.midiPlayer.play({ url: item.midiFile })
-                           }
-                       >
-                        ▶️
-                    </Button>
-                    <SwitchField
-                      label="Mute"
-                      isChecked={item.muted}
-                      onChange={(e) => {
-                        item.muted = e.target.checked;
-                      }}
-                      />
-                  </Flex>
-                  <Divider padding="xs" />
-                  <Heading padding="medium">{item.trackName}</Heading>
-                  <Flex>
-                    <Link color="#007EB9" href={item.midiFile}>
-                      Download
-                    </Link>
-                  </Flex>              
-                </View>
-              </Card>
-            )}
-          </Collection>
-        </Flex>
-        <Divider
-          orientation="horizontal" />
+        <Heading level={4}>Midi Output</Heading>
         <Flex>
-          <Link color="#007EB9" href="https://signal.vercel.app/">
-            <b>Signal</b> - a fantastic Online MIDI Editor
+          <Button onClick={async () => {
+            setMidiItems([]);
+            setMidiMasterFile(null);
+            setMidiFile(null);
+            setMidiTrackReady(false);
+            const result = await Storage.list('output/', {
+              level: 'private',
+              type: 'audio/midi'
+            })
+            console.log(result);
+            console.log('result1');
+            result.results.forEach(item => {
+              loadMidiFile(item);
+            })
+            
+            console.log('result2');
+            playOnLoad();
+          }}>Load MIDI file</Button>
+          <SwitchField
+            label="Play OnLoad"
+            isChecked={midiPlayOnLoad}
+            onChange={(e) => {
+              setMidiPlayOnLoad(e.target.checked);
+            }}
+            />
+        </Flex>   
+        {midiItems.length > 0
+        ? <Flex>  
+            <SelectField
+              label="Midi Tracks"
+              onChange={(e) => playMidiTrack(e.target.value)}
+              options={midiItems.map(item => item.trackName)}
+            ></SelectField>  
+          </Flex>  
+        : <Flex></Flex>
+      }  
+      {midiTrackReady
+        ? 
+        <Flex direction="column" gap="1rem" alignItems="center">
+          <MidiPlayer src={midiFile} />
+          <Link color="#007EB9" href={midiFile}>
+            Download
           </Link>
         </Flex>
-        <Divider
-          orientation="horizontal" />
+        : ""}
+      </Flex>     
+      <Divider
+        orientation="horizontal" />
+      <Flex>
+        <Link color="#007EB9" href="https://signal.vercel.app/">
+          <b>Signal</b> - a fantastic Online MIDI Editor
+        </Link>
+      </Flex>
+      <Divider
+        orientation="horizontal" />
     </Flex>
     );
 }
